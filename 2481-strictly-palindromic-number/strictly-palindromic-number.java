417c0ae63085bc6a997560bf90a0ffed9498a7be
akshaynalam40 <akshaynalam40@gmail.com>
2025-09-23 18:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (84.85%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,6 +1,14 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
-        
+        int base=2;
+        while(base<=n-2){
+            String s=Integer.toString(n);
+            String str= new StringBuilder(Integer.toString(n,base)).reverse().toString();
+            if(!s.equals(str)){
+                return false;
+            }
+            base++;
+        }
+        return true;
     }
 }
