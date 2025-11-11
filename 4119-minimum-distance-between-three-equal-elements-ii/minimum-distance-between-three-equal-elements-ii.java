0576e93cd51acb4157007f48b8d923bfbcb2e8ce
akshaynalam40 <akshaynalam40@gmail.com>
2025-11-11 18:26:04 +0530

    Time: 113 ms (48.25%) | Memory: 274 MB (64.21%) - LeetSync

---
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+
+        Map<Integer,ArrayList<Integer>> map = new HashMap<>();
+
+        for(int i = 0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                ArrayList<Integer> arr = new ArrayList<>();
+                arr.add(i);
+                map.put(nums[i],arr);
+            }
+            else{
+                map.get(nums[i]).add(i);
+            }
+        }
+
+        int minDistance = Integer.MAX_VALUE;
+        for(Integer key : map.keySet()){
+            ArrayList<Integer> arr = map.get(key);
+            
+            if(arr.size() < 3)continue;
+
+            for(int i = 0;i<=arr.size()-3;i++){
+                int first = arr.get(i);
+                int second = arr.get(i+1);
+                int third = arr.get(i+2);
+                
+                int curr = Math.abs(first-second)+ Math.abs(second-third)+Math.abs(third-first);
+                minDistance = Math.min(curr,minDistance);
+            }
+        }
+
+        if(minDistance == Integer.MAX_VALUE)return -1;
+        return minDistance;
+    }
+}
