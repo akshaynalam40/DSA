a9f801a17d7f27ed5e40938cf4550837a7305dea
akshaynalam40 <akshaynalam40@gmail.com>
2026-01-04 17:49:49 +0530

    Time: 25 ms (47.22%) | Memory: 48.3 MB (5.30%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -1,41 +1,45 @@
 class Solution {
     public int sumFourDivisors(int[] nums) {
-        int n=nums.length;
-        HashMap<Integer,Integer>hm= new HashMap<>();
+        int n = nums.length;
+        HashMap<Integer, Integer>hm = new HashMap<>();
         Arrays.sort(nums);
-        int res=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]<=4){
-                continue;
-            }
+        int res = 0;
+        for(int i = 0; i < n; i++){
             if(hm.containsKey(nums[i])){
-                res+=hm.get(nums[i]);
-            }
+                res += hm.get(nums[i]);
+            } 
             else{
-            ArrayList<Integer>al=new ArrayList<>();
-            int count=0;
-            for(int j=1;j<=nums[i];j++){
-               if(nums[i]%j==0){
-                al.add(j);
-                count++;
-               }
-               if(count==5){
-                break;
-               }
-               
-            }
-             int sum=0;
-            if(al.size()==4){
-                sum+=al.get(0);
-                sum+=al.get(1);
-                sum+=al.get(2);
-                sum+=al.get(3);
-            }
-            hm.put(nums[i],sum);
-            res+=sum;
-        }
-        }
+                ArrayList<Integer> al = new ArrayList<>();
+                int count = 0;
+                int num = nums[i];
+                for(int j = 1;j*j <= num;j++){
+                    if (num % j == 0) {
+                        int d1 = j;
+                        int d2 = num/j;
+                        al.add(d1);
+                        count++;
+                        if(d1!=d2) {
+                            al.add(d2);
+                            count++;
+                        }
+                    }
+                    if(count>4){
+                        break;
+                    }
+                }
 
+                int sum = 0;
+                if(al.size() == 4){
+                    sum += al.get(0);
+                    sum += al.get(1);
+                    sum += al.get(2);
+                    sum += al.get(3);
+                }
+
+                hm.put(num, sum);
+                res += sum;
+            }
+        }
         return res;
     }
 }
