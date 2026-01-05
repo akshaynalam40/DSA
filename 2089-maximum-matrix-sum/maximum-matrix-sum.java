70feb8087a59077ba841dc6f2119e9bd1e964742
akshaynalam40 <akshaynalam40@gmail.com>
2026-01-05 16:47:13 +0530

    Time: 34 ms (19.39%) | Memory: 72.4 MB (6.67%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -4,26 +4,22 @@ class Solution {
         int arr[] = new int[n * n];
         int k = 0;
         int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] < 0) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] < 0) {
                     count++;
                 }
                 arr[k++] = Math.abs(matrix[i][j]);
             }
         }
-
         Arrays.sort(arr);
-
         long sum = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
-        if (count % 2 != 0) {
-            sum -= 2L * arr[0];
+        if(count%2!=0) {
+            sum-=2*arr[0];
         }
-
         return sum;
     }
 }
